fed5a5fb5ca7e28bac2695826a9484a97318b3fe
Yessimbek Galymzhan <esm.2001@mail.ru>
2024-06-07 21:34:42 +0500

    classwork

---
+++ 06.07.2024/Galymone/yoyo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Class(models.Model):
+    count_of_stud = models.IntegerField()
+    created_at = models.DateTimeField()
+    class_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return  f'count of stud = {self.count_of_stud}, ClassName: {self.class_name}'
